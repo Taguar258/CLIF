7a01d1a0bc0ef5d03ef7f81774d1f3bd4dcf644f
Taguar258 <36562445+Taguar258@users.noreply.github.com>
2020-09-04 14:30:28 +0200

    Update template.py

--- modules/template.py
+++ modules/template.py
@@ -1,18 +1,20 @@
 from CLIF_Framework.framework import event
+from CLIF_Framework.framework import tools
 event = event()
+tools = tools()
 
 class Temp:
    def __init__(selfie, console):
-      global self
-      global var
-      self = selfie
-      var = console
-      
-      self._main()
+            global self
+            global var
+            self = selfie
+            var = console
+            
+            self._main()
    
-   def _main(self):
-      # Put your event help, etc. in here.
-      pass
+      def _main(self):
+            # Put your event help, etc. in here.
+            pass
 
 def setup(console):
-   console.add(Temp(console), event)
+      console.add(Temp(console), event)
